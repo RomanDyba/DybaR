97d37ec43e79eb5bbefcf5ebeb7b7e76de90aee2
RomanDyba <124334994+RomanDyba@users.noreply.github.com>
2023-02-14 11:11:44 +0300

    Практическая 1   14.02.2023

---
+++ Практическая 1.py
@@ -0,0 +1,15 @@
+tseloe = int(10)
+drobnoe = int(8.4)
+stroka = ("NO")
+
+big_netseloe = tseloe * 3.5
+drobnoe = drobnoe - 1
+print(tseloe // drobnoe)
+print(big_netseloe // drobnoe)
+
+stroka = ("NoNO_YesYesYes")
+
+print(tseloe)
+print(drobnoe)
+print(big_netseloe)
+print(stroka)
