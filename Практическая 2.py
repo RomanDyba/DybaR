36f2c82e8b119d2b055ee97a80fa170e9ccedc63
RomanDyba <124334994+RomanDyba@users.noreply.github.com>
2023-02-14 11:44:11 +0300

    Практическая работа 2 14.02.2023

---
+++ Практическая 2.py
@@ -0,0 +1,64 @@
+a = int(input())
+b = int(input())
+
+if (a + b)* a and (b - a)* b :
+    print("True")
+else:
+    print("False")
+
+if a * b == 0 and a / b == 3:
+     print("True")
+else:
+    print("False")
+if a - b == 1 and  (a + b) * b == 1:
+    print("True")
+else:
+    print("False")
+
+if a * b   and a // b:
+     print("True")
+else:
+    print("False")    
+    
+    
+    
+if (a + b)* a or (b - a)* b:
+    print("True")
+else:
+    print("False")
+
+if a * b == 0 or a / b == 3:
+     print("True")
+else:
+    print("False")
+if a - b == 1 or  (a + b) * b == 1:
+    print("True")
+else:
+    print("False")
+
+if a * b   or a // b:
+     print("True")
+else:
+    print("False")
+    
+c = str(input())
+v = str(input())
+
+if (c + v)* c and (c - v)* v :
+    print("True")
+else:
+    print("False")
+
+if c * v == 0 and c / v == 3:
+     print("True")
+else:
+    print("False")
+if c - v == 1 and  (c + v) * v == 1:
+    print("True")
+else:
+    print("False")
+
+if c * v   and c // v:
+     print("True")
+else:
+    print("False")
